48d686525f60612907d3bcbc238f7be09e083c03
Batool Kazmi <batoolkazmi@trentu.ca>
2024-07-16 23:03:10 -0400

    pagination completed

--- assn3-build/src/components/Pagination.jsx
+++ assn3-build/src/components/Pagination.jsx
@@ -2,19 +2,49 @@ import React from 'react'
 import "../styles/Pagination.css"
 import PaginationItem from './PaginationItem';
 
+
+const getPagesCut = ({ pageCount, pagesCutCount, currentPage }) => {
+    const ceiling = Math.ceil(pagesCutCount / 2);
+    const floor = Math.floor(pagesCutCount / 2);
+
+    if (pageCount < pagesCutCount) {
+        return {
+            start: 1, end: pageCount + 1
+        };
+    } else if (currentPage >= 1 && currentPage <= ceiling) {
+        return {
+            start: 1, end: pagesCutCount + 1
+        };
+    } else if (currentPage + floor >= pageCount) {
+        return {
+            start: pageCount - pagesCutCount + 1, end: pageCount + 1
+        };
+    } else {
+        return {
+            start: currentPage - ceiling + 1,
+            end: currentPage + floor + 1
+        };
+    }
+}
+
 function Pagination({ currentPage, total, limit, onPageChange }) {
 
     const pageCount = Math.ceil(total / limit);
+    const pagesCut = getPagesCut({ pageCount, pagesCutCount: 5, currentPage });
 
     let pages = [];
 
-    for (var i = 1; i <= pageCount; i++) {
+    for (var i = pagesCut.start; i <= pagesCut.end - 1; i++) {
         pages.push(i);
     }
-    console.log("pagesCount", pageCount, pages);
 
     return (
         <>
+            <PaginationItem
+                page='<<'
+                currentPage={currentPage}
+                onPageChange={() => onPageChange(1)}
+            />
             <PaginationItem
                 page='<'
                 currentPage={currentPage}
@@ -32,6 +62,11 @@ function Pagination({ currentPage, total, limit, onPageChange }) {
                 page='>'
                 currentPage={currentPage}
                 onPageChange={currentPage >= pageCount ? () => onPageChange(currentPage) : () => onPageChange(currentPage + 1)}
+            />
+            <PaginationItem
+                page='>>'
+                currentPage={currentPage}
+                onPageChange={() => onPageChange(pageCount)}
             />
         </>
     );
