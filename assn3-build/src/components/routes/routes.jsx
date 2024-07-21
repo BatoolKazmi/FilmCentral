83bc8fb3afeaf45a0d0bfd32aa3f69c970422ad0
“Shelmah” <shelmahkipngetich@trentu.ca>
2024-07-21 02:53:51 -0400

    Added more routes

--- assn3-build/src/components/routes/routes.jsx
+++ assn3-build/src/components/routes/routes.jsx
@@ -2,26 +2,36 @@ import Home from "./Home";
 import ErrorPage from "./Error";
 import ToWatchList from "./ToWatchList";
 import CompletedWatchList from "./CompletedWatchList";
+import CompletedMovies from "./CompletedMovies";
 import Movie from "./Movie";
+import ToWatchListMovies from "./TowatchlistMovies";
 
 const routes = [
-    {
-        path: "/",
-        element: <Home />,
-        errorElement: <ErrorPage />
-    },
-    {
-        path: "/towatchlist",
-        element: <ToWatchList />
-    },
-    {
-        path: "/completedwatchlist",
-        element: <CompletedWatchList />
-    },
-    {
-        path: "/movies/:id",
-        element: <Movie />
-    }
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/towatchlist",
+    element: <ToWatchList />,
+  },
+  {
+    path: "/completedwatchlist",
+    element: <CompletedWatchList />,
+  },
+  {
+    path: "/movies/:id",
+    element: <Movie />,
+  },
+  {
+    path: "/completedwatchlist/entries/:id/:key",
+    element: <CompletedMovies />,
+  },
+  {
+    path: "/towatchlist/entries/:id/:key",
+    element: <ToWatchListMovies />,
+  },
 ];
 
 export default routes;
