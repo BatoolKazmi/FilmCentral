2d424019b7cb4a2885060ab87ac320d5fa10a7bd
Batool Kazmi <kazmib6@gmail.com>
2024-11-26 18:46:24 -0500

    changed backend authentic

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,17 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 
-// })
+})
 
-const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
-const db = mysql.createConnection(urlDB);
+// const db = mysql.createConnection(urlDB);
 
 db.connect((err) => {
     if (err) {
