329146c6a2efa17e62e34e06282b90bdd6ed9d56
Batool Kazmi <kazmib6@gmail.com>
2024-11-27 01:32:15 -0500

    added ssl connection

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import axios from "axios";
 import mysql from "mysql2";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import dotenv from 'dotenv';
 dotenv.config();
@@ -17,7 +17,10 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true, // Change as needed
+    },
 })
 
 // const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
