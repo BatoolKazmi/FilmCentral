9340e54ca51e2c9d6ef6a9af36a703fe30951b1e
Batool Kazmi <kazmib6@gmail.com>
2024-11-27 00:44:08 -0500

    fixed issues with connecting to clever cloud database

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.PORT, 
-
+    database: process.env.DB_NAME
 })
 
 // const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
