900537d8abd6751f013bb489d93561f6257d6a30
Batool Kazmi <kazmib6@gmail.com>
2024-12-02 01:26:07 -0500

    added port

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ const db = mysql.createPool({
     user: process.env.MYSQL_ADDON_USER,
     password: process.env.MYSQL_ADDON_PASSWORD,
     database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT,
     waitForConnections: true,
     connectionLimit: 5,
 });
